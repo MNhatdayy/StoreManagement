709336517761e16b78c8a5f8f785c8d6294888c7
MinhNhat2003 <nhatnm2003@gmail.com>
2024-04-09 16:57:01 +0700

    pull code

---
+++ DTO/OrderDetailDTO.cs
@@ -0,0 +1,15 @@
+﻿using StoreManagement.Models;
+
+namespace StoreManagement.DTO
+{
+    public class OrderDetailDTO
+    {
+        public int FoodId { get; set; }
+        public FoodItemDTO FoodItem { get; set; }
+        public int OrderId { get; set; }
+        public OrderDTO Order { get; set; }
+        public int Quantity { get; set; } = 1;
+        public string Notes { get; set; }
+        public double Price { get; set; }
+    }
+}
